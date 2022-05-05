b38546ac026713ff19d1da1865bb18c2339efc3e
leiradel <andre@leiradella.com>
2022-05-05 20:47:31 +0100

    Added support for vectors in lutil

--- src/bindings/lutil.c
+++ src/bindings/lutil.c
@@ -36,3 +36,130 @@ void lutil_regintconsts(lua_State* const L, char const* const name, lutil_IntCon
         lua_setfield(L, -2, name);
     }
 }
+
+static int array_set(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+
+    lua_Integer const ndx = luaL_checkinteger(L, 2);
+    void* const element = vector_at(self, ndx - 1);
+
+    if (element == NULL) {
+        return luaL_error(L, "index %I out of bounds", ndx);
+    }
+
+    desc->init_element(L, element, 3);
+    return 0;
+}
+
+static int array_get(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+
+    lua_Integer const ndx = luaL_checkinteger(L, 2);
+    void const* const element = vector_at(self, ndx - 1);
+
+    if (element == NULL) {
+        return luaL_error(L, "index %I out of bounds", ndx);
+    }
+
+    return desc->push_element_fields(L, element);
+}
+
+static int array_append(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+
+    void* const element = vector_append(self);
+
+    if (element == NULL) {
+        return luaL_error(L, "out of memory");
+    }
+
+    desc->init_element(L, element, 2);
+    return 0;
+}
+
+static int array_reserve(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+
+    lua_Integer const new_capacity = luaL_checkinteger(L, 2);
+
+    if (vector_reserve(self, new_capacity) != 0) {
+        return luaL_error(L, "out of memory");
+    }
+
+    return 0;
+}
+
+static int array_resize(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+
+    lua_Integer const new_count = luaL_checkinteger(L, 2);
+
+    if (vector_resize(self, new_count) != 0) {
+        return luaL_error(L, "out of memory");
+    }
+
+    return 0;
+}
+
+static int array_shrink(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+    vector_shrink(self);
+    return 0;
+}
+
+static int array_len(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+    lua_pushinteger(L, self->count);
+    return 1;
+}
+
+static int array_gc(lua_State* const L) {
+    lutil_VectorDesc const* const desc = lua_touserdata(L, lua_upvalueindex(1));
+    Vector* const self = lutil_check_array(desc, L, 1);
+    vector_destroy(self);
+    return 0;
+}
+
+Vector* lutil_check_array(lutil_VectorDesc const* const desc, lua_State* const L, int const ndx) {
+    return luaL_checkudata(L, ndx, desc->metatable_name);
+}
+
+Vector* lutil_push_array(lutil_VectorDesc const* const desc, lua_State* const L) {
+    Vector* self = lua_newuserdata(L, sizeof(Vector));
+    vector_init(self, desc->element_size);
+
+    if (luaL_newmetatable(L, desc->metatable_name) != 0) {
+        static luaL_Reg const methods[] = {
+            {"set", array_set},
+            {"get", array_get},
+            {"append", array_append},
+            {"reserve", array_reserve},
+            {"resize", array_resize},
+            {"shrink", array_shrink},
+            {NULL, NULL}
+        };
+
+        luaL_newlibtable(L, methods);
+        lua_pushlightuserdata(L, (void*)desc);
+        luaL_setfuncs(L, methods, 1);
+        lua_setfield(L, -2, "__index");
+
+        lua_pushlightuserdata(L, (void*)desc);
+        lua_pushcclosure(L, array_len, 1);
+        lua_setfield(L, -2, "__len");
+
+        lua_pushlightuserdata(L, (void*)desc);
+        lua_pushcclosure(L, array_gc, 1);
+        lua_setfield(L, -2, "__gc");
+    }
+
+    lua_setmetatable(L, -2);
+    return self;
+}
--- src/bindings/lutil.h
+++ src/bindings/lutil.h
@@ -1,6 +1,8 @@
 #ifndef LUTIL_H__
 #define LUTIL_H__
 
+#include "vector.h"
+
 #include <lua.h>
 
 typedef struct {
@@ -9,7 +11,18 @@ typedef struct {
 }
 lutil_IntConst;
 
+typedef struct {
+    char const* const metatable_name;
+    size_t const element_size;
+    void (* const init_element)(lua_State* const L, void* const element, int const ndx);
+    int (* const push_element_fields)(lua_State* const L, void const* const element);
+}
+lutil_VectorDesc;
+
 int lutil_pcall(lua_State* const L, int const nargs, int const nres);
 void lutil_regintconsts(lua_State* const L, char const* const name, lutil_IntConst const* const consts, size_t const count);
+
+Vector* lutil_check_array(lutil_VectorDesc const* const desc, lua_State* const L, int const ndx);
+Vector* lutil_push_array(lutil_VectorDesc const* const desc, lua_State* const L);
 
 #endif /* LUTIL_H__ */
