d4de192ee90ba298fae4bcb617054091c15c6bcd
leiradel <andre@leiradella.com>
2022-05-06 19:25:11 +0100

    Implemented __newindex for sg_desc

--- src/bindings/gfx.c
+++ src/bindings/gfx.c
@@ -1,3 +1,5 @@
+#include "gfx.h"
+
 #include <string.h>
 
 #include <lua.h>
@@ -22,74 +24,208 @@
 
 #define SG_DESC_MT "sg_desc"
 
-typedef struct {
-    sg_desc desc;
-}
-Desc;
+static void* free_descs = NULL;
 
-static Desc* desc_check(lua_State* const L, int const ndx) {
+SgDesc* sg_desc_check(lua_State* const L, int const ndx) {
     return luaL_checkudata(L, ndx, SG_DESC_MT);
 }
 
-#if 0
-static int desc_index(lua_State* const L) {
-    Desc const* const self = desc_check(L, 1);
+static int sg_desc_index(lua_State* const L) {
+    SgDesc const* const self = sg_desc_check(L, 1);
     char const* const key = luaL_checkstring(L, 2);
     djb2_hash const hash = djb2(key);
 
     switch (hash) {
         case DJB2HASH_C(0x42d88132): /* buffer_pool_size */
-            lua_pushinteger(L, self->desc.buffer_pool_size);
+            lua_pushinteger(L, self->data.buffer_pool_size);
             return 1;
 
         case DJB2HASH_C(0x789879bb): /* image_pool_size */
-            lua_pushinteger(L, self->desc.image_pool_size);
+            lua_pushinteger(L, self->data.image_pool_size);
             return 1;
 
         case DJB2HASH_C(0x00c03f2f): /* shader_pool_size */
-            lua_pushinteger(L, self->desc.shader_pool_size);
+            lua_pushinteger(L, self->data.shader_pool_size);
             return 1;
 
         case DJB2HASH_C(0x03a9b7ee): /* pipeline_pool_size */
-            lua_pushinteger(L, self->desc.pipeline_pool_size);
+            lua_pushinteger(L, self->data.pipeline_pool_size);
             return 1;
 
         case DJB2HASH_C(0x3bc833ef): /* pass_pool_size */
-            lua_pushinteger(L, self->desc.pass_pool_size);
+            lua_pushinteger(L, self->data.pass_pool_size);
             return 1;
 
         case DJB2HASH_C(0x89372a5d): /* context_pool_size */
-            lua_pushinteger(L, self->desc.context_pool_size);
+            lua_pushinteger(L, self->data.context_pool_size);
             return 1;
 
         case DJB2HASH_C(0x73889538): /* uniform_buffer_size */
-            lua_pushinteger(L, self->desc.uniform_buffer_size);
+            lua_pushinteger(L, self->data.uniform_buffer_size);
             return 1;
 
         case DJB2HASH_C(0x7ed6f945): /* staging_buffer_size */
-            lua_pushinteger(L, self->desc.staging_buffer_size);
+            lua_pushinteger(L, self->data.staging_buffer_size);
             return 1;
 
         case DJB2HASH_C(0x7b524926): /* sampler_cache_size */
-            lua_pushinteger(L, self->desc.sampler_cache_size);
+            lua_pushinteger(L, self->data.sampler_cache_size);
+            return 1;
+
+        case DJB2HASH_C(0xd3799aca): /* context */
+            sg_context_desc_push(L)->data = self->data.context;
             return 1;
     }
 
     return luaL_error(L, "unknown field %s in %s", key, SG_DESC_MT);
 }
 
-static Desc* desc_push(lua_State* const L) {
-    Desc* const self = lua_newuserdata(L, sizeof(*self));
+static int sg_desc_newindex(lua_State* const L) {
+    SgDesc* const self = sg_desc_check(L, 1);
+    char const* const key = luaL_checkstring(L, 2);
+    djb2_hash const hash = djb2(key);
 
-    if (luaL_newmetatable(L, SG_DESC_MT) != 0) {
-        lua_pushcfunction(L, desc_index);
-        lua_setfield(L, -2, "__index");
+    switch (hash) {
+        case DJB2HASH_C(0x42d88132): /* buffer_pool_size */
+            self->data.buffer_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x789879bb): /* image_pool_size */
+            self->data.image_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x00c03f2f): /* shader_pool_size */
+            self->data.shader_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x03a9b7ee): /* pipeline_pool_size */
+            self->data.pipeline_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x3bc833ef): /* pass_pool_size */
+            self->data.pass_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x89372a5d): /* context_pool_size */
+            self->data.context_pool_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x73889538): /* uniform_buffer_size */
+            self->data.uniform_buffer_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x7ed6f945): /* staging_buffer_size */
+            self->data.staging_buffer_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0x7b524926): /* sampler_cache_size */
+            self->data.sampler_cache_size = luaL_checkinteger(L, 3);
+            return 0;
+
+        case DJB2HASH_C(0xd3799aca): /* context */
+            self->data.context = sg_context_desc_check(L, 3)->data;
+            return 0;
     }
 
-    lua_setmetatable(L, -2);
+    return luaL_error(L, "unknown field %s in %s", key, SG_DESC_MT);
+}
+
+static int sg_desc_gc(lua_State* const L) {
+    SgDesc* const self = sg_desc_check(L, 1);
+    lutil_collect_cached((lutil_CachedObject*)self, L, &free_descs);
+    return 0;
+}
+
+SgDesc* sg_desc_push(lua_State* const L) {
+    bool setmt = false;
+    SgDesc* self = (SgDesc*)lutil_push_cached(L, &free_descs, sizeof(*self), &setmt);
+
+    if (setmt) {
+        if (luaL_newmetatable(L, SG_DESC_MT) != 0) {
+            lua_pushcfunction(L, sg_desc_index);
+            lua_setfield(L, -2, "__index");
+
+            lua_pushcfunction(L, sg_desc_newindex);
+            lua_setfield(L, -2, "__newindex");
+
+            lua_pushcfunction(L, sg_desc_gc);
+            lua_setfield(L, -2, "__gc");
+        }
+
+        lua_setmetatable(L, -2);
+    }
+
     return self;
 }
-#endif
+
+static int l_new_desc(lua_State* const L) {
+    SgDesc* const self = sg_desc_push(L);
+    memset(&self->data, 0, sizeof(self->data));
+    return 1;
+}
+
+/*
+ ######   ######            ######   #######  ##    ## ######## ######## ##     ## ########         ########  ########  ######   ######  
+##    ## ##    ##          ##    ## ##     ## ###   ##    ##    ##        ##   ##     ##            ##     ## ##       ##    ## ##    ## 
+##       ##                ##       ##     ## ####  ##    ##    ##         ## ##      ##            ##     ## ##       ##       ##       
+ ######  ##   ####         ##       ##     ## ## ## ##    ##    ######      ###       ##            ##     ## ######    ######  ##       
+      ## ##    ##          ##       ##     ## ##  ####    ##    ##         ## ##      ##            ##     ## ##             ## ##       
+##    ## ##    ##          ##    ## ##     ## ##   ###    ##    ##        ##   ##     ##            ##     ## ##       ##    ## ##    ## 
+ ######   ######   #######  ######   #######  ##    ##    ##    ######## ##     ##    ##    ####### ########  ########  ######   ######  
+*/
+
+#define SG_CONTEXT_DESC_MT "sg_context_desc"
+
+static void* free_context_descs = NULL;
+
+SgContextDesc* sg_context_desc_check(lua_State* const L, int const ndx) {
+    return luaL_checkudata(L, ndx, SG_CONTEXT_DESC_MT);
+}
+
+static int sg_context_desc_index(lua_State* const L) {
+    SgContextDesc const* const self = sg_context_desc_check(L, 1);
+    char const* const key = luaL_checkstring(L, 2);
+    djb2_hash const hash = djb2(key);
+
+    switch (hash) {
+        case DJB2HASH_C(0x0c91394c): /* color_format */
+            lua_pushinteger(L, self->data.color_format);
+            return 1;
+        case DJB2HASH_C(0x23ebaae2): /* depth_format */
+            lua_pushinteger(L, self->data.depth_format);
+            return 1;
+        case DJB2HASH_C(0x7c5fd34f): /* sample_count */
+            lua_pushinteger(L, self->data.sample_count);
+            return 1;
+    }
+
+    return luaL_error(L, "unknown field %s in %s", key, SG_CONTEXT_DESC_MT);
+}
+
+static int sg_context_desc_gc(lua_State* const L) {
+    SgContextDesc* const self = sg_context_desc_check(L, 1);
+    lutil_collect_cached((lutil_CachedObject*)self, L, &free_context_descs);
+    return 0;
+}
+
+SgContextDesc* sg_context_desc_push(lua_State* const L) {
+    bool setmt = false;
+    SgContextDesc* self = (SgContextDesc*)lutil_push_cached(L, &free_descs, sizeof(*self), &setmt);
+
+    if (setmt) {
+        if (luaL_newmetatable(L, SG_CONTEXT_DESC_MT) != 0) {
+            lua_pushcfunction(L, sg_context_desc_index);
+            lua_setfield(L, -2, "__index");
+
+            lua_pushcfunction(L, sg_context_desc_gc);
+            lua_setfield(L, -2, "__gc");
+        }
+
+        lua_setmetatable(L, -2);
+    }
+
+    return self;
+}
 
 /*
  ######   ######           ########     ###     ######   ######             ###     ######  ######## ####  #######  ##    ## 
@@ -209,8 +345,8 @@ static int l_new_pass_action(lua_State* const L) {
 */
 
 static int l_setup(lua_State* const L) {
-    Desc const* const desc = desc_check(L, 1);
-    sg_setup(&desc->desc);
+    SgDesc const* const desc = sg_desc_check(L, 1);
+    sg_setup(&desc->data);
     return 0;
 }
 
@@ -247,18 +383,6 @@ static int l_commit(lua_State* const L) {
     return 0;
 }
 
-// From sokol_glue
-static int l_setup_from_app(lua_State* const L) {
-    (void)L;
-
-    sg_desc const desc = {
-        .context = sapp_sgcontext()
-    };
-
-    sg_setup(&desc);
-    return 0;
-}
-
 /*
 ##       ##     ##    ###     #######  ########  ######## ##    ##          ######   #######  ##    ##  #######  ##                ######   ######## ##     ## 
 ##       ##     ##   ## ##   ##     ## ##     ## ##       ###   ##         ##    ## ##     ## ##   ##  ##     ## ##               ##    ##  ##        ##   ##  
@@ -271,6 +395,7 @@ static int l_setup_from_app(lua_State* const L) {
 
 LUAMOD_API int luaopen_sokol_gfx(lua_State* const L) {
     static luaL_Reg const functions[] = {
+        {"new_desc", l_new_desc},
         {"new_pass_action", l_new_pass_action},
         {"setup", l_setup},
         {"shutdown", l_shutdown},
@@ -278,7 +403,6 @@ LUAMOD_API int luaopen_sokol_gfx(lua_State* const L) {
         {"begin_default_pass", l_begin_default_pass},
         {"end_pass", l_end_pass},
         {"commit", l_commit},
-        {"setup_from_app", l_setup_from_app},
 
         {NULL, NULL}
     };
